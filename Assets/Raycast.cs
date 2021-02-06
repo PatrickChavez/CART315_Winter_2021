2536b7914c753e105fd9d08676517d0e8a6b5419
Patrick <hellohello@hello.com>
2021-02-06 13:11:12 -0500

    Added raycast, probuilder and lighting effects

--- Assets/Raycast.cs
+++ Assets/Raycast.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class raycast : MonoBehaviour
+public class Raycast : MonoBehaviour
 {
     public Camera maincamera;
     public GameObject prefab;
